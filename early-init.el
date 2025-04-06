b2eb1733566ccccbca606b931539a894e2ec1bd6
du-toit <rickz75dev@gmail.com>
2025-04-06 10:57:09 +0200

    fix an issue of emacs flashing white when opened

--- early-init.el
+++ early-init.el
@@ -1,3 +1,17 @@
+;; -------------------------------------------------------------------
+;; Window Settings
+;; -------------------------------------------------------------------
+
+;; Setting the face in here prevents flashes of
+;; color as the theme gets activated
+
+(setq default-frame-alist '(
+      (background-color . "#2e3440")
+      (ns-appearance . dark)
+      (ns-transparent-titlebar . t)))
+
+;; -------------------------------------------------------------------
+
 (provide 'early-init)
 
 ;;; early-init.el ends here
