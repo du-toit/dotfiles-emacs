10954cfbd3bb192fa064e42598a251d3a37a3c10
du-toit <rickz75dev@gmail.com>
2025-04-06 12:11:51 +0200

    Update early-init.el

--- early-init.el
+++ early-init.el
@@ -3,13 +3,15 @@
 ;; color as the theme gets activated
 (setq default-frame-alist '(
       (background-color . "#282a36")
-      (ns-appearance . dark)))
+      (ns-appearance . dark)
+      (ns-transparent-titlebar . t)))
 
 ;; Setting the face in here prevents flashes of
 ;; color as the theme gets activated
 (setq initial-frame-alist '(
       (background-color . "#282a36")
-      (ns-appearance . dark)))
+      (ns-appearance . dark)
+      (ns-transparent-titlebar . t)))
 
 (provide 'early-init)
 
