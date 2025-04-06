f7b86be06d8d16d82537d72f2f2f1a18c8e302ec
du-toit <dutoit@dutoit-linux>
2025-04-06 10:40:36 +0200

    change where the packages and cache is stored

--- init.el
+++ init.el
@@ -1,3 +1,31 @@
+;; -------------------------------------------------------------------------------------------------------
+;; Define our Emacs configuration structure
+;; -------------------------------------------------------------------------------------------------------
+
+(defconst directory-root (file-name-directory user-emacs-directory)
+  "The root directory of the Emacs configuration.")
+
+(defconst directory-core (concat directory-root "core")
+  "Global settings and core functionality.")
+
+(defconst directory-settings (concat directory-root "settings")
+  "Language-specific and package-specific setups and tweaks.")
+
+;; -------------------------------------------------------------------------------------------------------
+;; Add our configuration directories to our Emacs load path
+;; -------------------------------------------------------------------------------------------------------
+
+(add-to-list 'load-path directory-core)
+(add-to-list 'load-path directory-settings)
+
+;; -------------------------------------------------------------------------------------------------------
+;; Load the core configuration
+;; -------------------------------------------------------------------------------------------------------
+
+(require 'core-path)
+
+;; -------------------------------------------------------------------------------------------------------
+
 (provide 'init)
 
 ;;; init.el ends here
